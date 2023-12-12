56f7805cde5d808bc7e287c4497fca231775d070
ket <152324264+ket03@users.noreply.github.com>
2023-12-12 14:36:46 +0300

    added urls and button-more, converter

--- main.go
+++ main.go
@@ -3,15 +3,46 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	// "fmt"
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
-// links to button more
-// var URL_SPAR = "https://myspar.ru" // +catalog
-var URL_SPAR = "https://myspar.ru/catalog/"
-var URL_MAGNIT = "https://magnit.ru/catalog/"
-var URL_LENTA = "https://lenta.com/catalog/"
+var URL_SPAR = "https://myspar.ru" // +catalog
+var URL_SPAR_CATALOG = "https://myspar.ru/catalog/"
+var SPAR_PATH_BUTTON_MORE = ".js-next-page"
+var SPAR_PATH_BUTTON_ALERT = ".js-modal-close"
+var SPAR_CUT = 6
+var SPAR_START = 1
+var SPAR_END = 1
 
+var URL_MAGNIT = "https://magnit.ru/catalog/"
+var MAGNIT_PATH_BUTTON_MORE = ".paginate__more"
+var MAGNIT_CUT = 4
+var MAGNIT_START = 3
+var MAGNIT_END = 2
+
+var URL_LENTA = "https://lenta.com/catalog/"
+var LENTA_PATH_BUTTON_MORE = ".button--primary catalog-grid__pagination-button"
+var LENTA_CUT = 0
+var LENTA_START = 0
+var LENTA_END = 0
+
+var URL_AUCHAN = "https://www.auchan.ru/catalog"
+var AUCHAN_PATH_BUTTON_MORE = ".showMoreButton"
+var AUCHAN_CUT = 0
+var AUCHAN_START = 0
+var AUCHAN_END = 0
+
+var URL_PYATEEROCHKA = "https://5ka.ru/rating/catalogue"
+var PYATEEROCHKA_PATH_BUTTON_MORE = ""
+var PYATEEROCHKA_CUT = 0
+var PYATEEROCHKA_START = 0
+var PYATEEROCHKA_END = 0
 
 func main() {
   opts := append(chromedp.DefaultExecAllocatorOptions[:],
@@ -22,47 +53,35 @@ func main() {
     chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"),
   )
   allocCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
-  ctx, _ := chromedp.NewContext(allocCtx)
-  // defer close()
-  // fmt.Println(getLinks(ctx))
-  // fmt.Print(magnit(ctx)) // один общий каталог
-  // fmt.Print(spar(ctx)) // много подкаталогов
-  // pyaterochka(ctx) // хз пока, ничего не работает
-  fmt.Print(lenta(ctx)) // не много подкаталогов
-  // auchan(ctx) // не много подкаталогов относительно спара
+  ctx, close := chromedp.NewContext(allocCtx)
+  defer close()
+  fmt.Println(spar(ctx))
+  // fmt.Println(magnit(ctx))
+  // fmt.Println(lenta(ctx))
+  // fmt.Println(auchan(ctx))
+  // fmt.Println(pyaterochka(ctx))
 }
 
-// func pyaterochka(ctx context.Context) bool {
-//   var res string
+func buttonMore(ctx context.Context, button_path string) {
+	var button []*cdp.Node
+	chromedp.Run(ctx,
+		chromedp.WaitVisible("body > footer"),
+		chromedp.Sleep(4*time.Second),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			if err := chromedp.Nodes(button_path, &button, chromedp.AtLeast(0)).Do(ctx); err != nil {
+				log.Fatal(err)
+			}
+			if len(button) > 0 {
+				chromedp.Click(button_path).Do(ctx)
+				buttonMore(ctx, button_path)
+			}
+			return nil
+		}))
+}
 
-//   if err := chromedp.Run(ctx,
-//     browser.GrantPermissions([]browser.PermissionType{browser.PermissionTypeGeolocation}).WithOrigin("https://5ka.ru/rating/catalogue"), // выключает уведомление геолокации
-//     chromedp.Navigate("https://5ka.ru/rating/catalogue"),
-//     chromedp.WaitVisible(".location-confirm__button"),
-//     chromedp.Click(".location-confirm__button"), // choose city
-//     chromedp.Click(".focus-btn__content"), // cookies
-//     // chromedp.Click(".js-next-page"), // more products
-//     // chromedp.Text(".catalog-list", &res),
-//   ); err != nil {
-//     log.Fatal(err, "упала пятерочка")
-//   }
-//   fmt.Println(res)
-//   return true
-// }
-
-// func auchan(ctx context.Context) bool {
-//   var res string
-//   if err := chromedp.Run(ctx,
-//     chromedp.Navigate("https://www.auchan.ru/catalog/ovoschi-frukty-zelen-griby-yagody/"),
-//     browser.GrantPermissions([]browser.PermissionType{browser.PermissionTypeGeolocation}).WithOrigin("https://www.auchan.ru/catalog/ovoschi-frukty-zelen-griby-yagody/"), // выключает уведомление геолокации
-//     chromedp.Click("css-14ci9uo css-m9m6cb"),
-//     chromedp.WaitVisible(".subcategoryContainer"),
-
-//     chromedp.Text(".rr-widget-5ede0ae197a52530444d9c33-popular", &res),
-//     // chromedp.Title(&res),
-//   ); err != nil {
-//     log.Fatal(err, "упал ашан")
-//   }
-//   fmt.Println(res)
-//   return true
-// }
+func convertStringToFloat(price string, cut int, start int, end int) float64 {
+  price = price[:len(price)-cut]
+	price = price[:len(price)-start] + "." + price[len(price)-end:]
+	res, _ := strconv.ParseFloat(price, 64)
+	return res
+}
